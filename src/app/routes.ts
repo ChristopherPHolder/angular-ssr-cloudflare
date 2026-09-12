fc0a10bf6f3242a2f0a2e69018b3b52ef7654e80
ChristopherPHolder <toholderandrews@gmail.com>
2026-09-12 22:34:45 +0200

    fix: default route to discover

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,38 +3,30 @@ import {Routes} from '@angular/router';
 export const ROUTES: Routes = [
   {
     path: 'list/:type/:identifier',
-    loadComponent: () =>
-      import('./pages/movie-list-page/movie-list-page.component'),
+    loadComponent: () => import('./pages/movie-list-page/movie-list-page.component'),
   },
   {
     path: 'detail/movie/:identifier',
-    loadComponent: () =>
-      import('./pages/movie-detail-page/movie-detail-page.component'),
+    loadComponent: () => import('./pages/movie-detail-page/movie-detail-page.component'),
   },
   {
     path: 'detail/list/:identifier',
-    loadChildren: () =>
-      import(
-        './pages/account-feature/list-detail-page/list-detail-page.routes'
-        ),
+    loadChildren: () => import('./pages/account-feature/list-detail-page/list-detail-page.routes'),
   },
   {
     path: 'detail/person/:identifier',
-    loadComponent: () =>
-      import('./pages/person-detail-page/person-detail-page.component'),
+    loadComponent: () => import('./pages/person-detail-page/person-detail-page.component'),
   },
   {
     path: 'account',
-    loadChildren: () =>
-      import('./pages/account-feature/account-feature-page.routes'),
+    loadChildren: () => import('./pages/account-feature/account-feature-page.routes'),
   },
   {
     path: 'page-not-found',
-    loadComponent: () =>
-      import('./pages/not-found-page/not-found-page.component'),
+    loadComponent: () => import('./pages/not-found-page/not-found-page.component'),
   },
   {
     path: '**',
-    redirectTo: 'page-not-found',
+    redirectTo: 'list/category/popular',
   },
 ];
